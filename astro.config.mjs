e4781190acac5798bb0fe6dd4b922dc8ebcf027e
Vishal Gaurav <lemeraemail@gmail.com>
2023-10-01 12:03:47 +0530

    fix: update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://vishalg8454.github.io/Astro-blog/',
 	integrations: [mdx(), sitemap()],
+	base: '/Astro-blog/'
 });
